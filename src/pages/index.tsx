03d9e078d2b70c1ab781859827efd8ed66b92630
Stephen Ajulu <alunje73@gmail.com>
2020-08-18 13:50:12 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,10 +26,8 @@ export default () => (
             You can say we have a need for speed 😉
     </Intro>
           
-          <hr width="75%">
-          <style type="text/css">
-@media screen and (max-width: 767px) {.tg {width: auto !important;}.tg col {width: auto !important;}.tg-wrap {overflow-x: auto;-webkit-overflow-scrolling: touch;margin: auto 0px;}}
-          </style>
+          <hr width="75%" />
+          
 <div class="tg-wrap"><table class="tg" border="0" cellpadding="5" cellspacing="0">
 <thead>
   <tr>
@@ -41,15 +39,15 @@ export default () => (
 <tbody>
   <tr>
     <td class="tg-0lax">A strong web presence attracts your target visitors and turns them into your new customers.
-<br />
+
                   But it’s paying attention to the detail that makes all the difference.
-<br />
+
                   A well-designed website is about far more than just its visual appearance.</td>
     <td class="tg-0lax">We help you shape your strategy by going through a Consultation Session.
-<br />
+      
 What comes out of the session is a clear understanding of your objectives, how to fix the present errors and what needs to be put in place to achieve your goals</td>
     <td class="tg-0lax">As we mentioned above we have a need for speed. Statistics state users will rarely spend more than 20 seconds waiting for all elements of a webpage to load before leaving.
-<br />
+
       A fast, User Friendly and Search Engine Optimized website boosts user engagement and lead generation </td>
   </tr>
 </tbody>
@@ -62,16 +60,16 @@ What comes out of the session is a clear understanding of your objectives, how t
                 <h5 align="left">Web Development and Design</h5>
                 <p align="left">
                   A strong web presence attracts your target visitors and turns them into your new customers.
-<br />
+
                   But it’s paying attention to the detail that makes all the difference.
-<br />
+
                   A well-designed website is about far more than just its visual appearance.
                 </p> 
                 
                 <h5 align="center">I.T and Cybersecurity Consultation</h5>
                 <p align="center">
                   We help you shape your strategy by going through a Consultation Session.
-<br />
+
                   What comes out of the session is a clear understanding of your objectives, how to fix the present errors and what needs to be put in place to achieve your goals
                 </p>
                 
@@ -79,7 +77,7 @@ What comes out of the session is a clear understanding of your objectives, how t
                 <p align="right">
                   
                 </p>
-          <br />
+          
                 For more informations go to{' '}
           <a href="https://ueno.co" target="_blank" rel="noopener noreferrer">
             ueno.co
