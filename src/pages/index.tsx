fb6b189ce04bc2bdbeaef7c88f15dc82cc0658c9
Stephen Ajulu <alunje73@gmail.com>
2020-08-30 19:34:24 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,12 +12,12 @@ export default () => (
     <Helmet title="Home" />
 
     <Intro>
-      OwlSec Technologies is a strategic I.T and Cybersecurity agency in Nairobi, Kenya.{' '} That builds&nbsp;
+      Paracy is a strategic I.T and Cybersecurity agency in Nairobi, Kenya.{' '} That builds&nbsp;
       <Highlight>beautiful&nbsp;digital&nbsp;products</Highlight>,&nbsp;<Highlight>brands</Highlight>&nbsp;and&nbsp;<Highlight>experiences</Highlight>
       <br />
       We help you create, grow and foster connections with your customers and users. And we believe we’re pretty good at it.
       <br />{' '}
-       <a href="https://owlsectechnologies4.netlify.app/about"  target="_blank" rel="noopener noreferrer">
+       <a href="https://paracy.netlify.app/about"  target="_blank" rel="noopener noreferrer">
          Learn more
       </a>
     </Intro>
@@ -30,7 +30,7 @@ export default () => (
       description={
         <>
           Let's Talk{' '}
-          <a href="https://owlsectechnologies4.netlify.app/contact" target="_blank" rel="noopener noreferrer">
+          <a href="mailto:paracy.co@gmail.com" target="_blank" rel="noopener noreferrer">
             Tell Us Everything
           </a>
           .
